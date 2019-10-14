00c2fe5e5f1bd2249108a2cc3fdbb4e86cf53118
Jesus E. Franco Martinez <jefrancomix@gmail.com>
2019-10-13 19:37:58 -0500

    Inicio docs, @deprecated curryToUnary a unary

--- src/functions.php
+++ src/functions.php
@@ -2,6 +2,11 @@
 namespace Apantle\FunPHP;
 const _ = 'curryUnaryArg';
 
+/**
+ * Creates a function that always returns the $value given
+ * @param $value
+ * @return callable
+ */
 function constant($value): callable
 {
     return function () use ($value) {
@@ -9,17 +14,33 @@ function constant($value): callable
     };
 }
 
+/**
+ * Retuns the same passed element
+ * @param $item
+ * @return mixed
+ */
 function identity($item)
 {
     return $item;
 }
 
+/**
+ * Returns the first item in the array
+ * @param array $items
+ * @return mixed
+ */
 function head(array $items)
 {
     return $items[0];
 }
 
-function compose(callable $f, callable $g): callable
+/**
+ * Returns a function where $g result is passed to $f function
+ * @param callable $f
+ * @param callable $g
+ * @return callable
+ */
+function compose($f, $g)
 {
     return function () use ($f, $g) {
         $fun_args = func_get_args();
@@ -27,6 +48,11 @@ function compose(callable $f, callable $g): callable
     };
 }
 
+/**
+ * La primera función puede admitir varios argumentos, las restantes son unary
+ * @param callable[] ...$functions array de funciones
+ * @return callable
+ */
 function pipe(...$functions): callable
 {
     return function() use($functions) {
@@ -42,7 +68,12 @@ function pipe(...$functions): callable
     };
 }
 
-function curryToUnary($callableFirst, ...$fun_args): callable
+/**
+ * @param callable $callableFirst función de soporte a ser llamada
+ * @param mixed ...$fun_args argumentos adicionales
+ * @return callable
+ */
+function unary($callableFirst, ...$fun_args): callable
 {
     if(in_array(_, $fun_args)) {
 
@@ -61,6 +92,23 @@ function curryToUnary($callableFirst, ...$fun_args): callable
     };
 }
 
+/**
+ * @deprecated change to unary simply
+ */
+function curryToUnary($callableFirst, ...$fun_args): callable
+{
+    $unaryArgs = array_reduce($fun_args, function ($acc, $arg) {
+        array_push($acc, $arg);
+        return $acc;
+    }, [$callableFirst]);
+    return call_user_func_array(__NAMESPACE__ . '\unary', $unaryArgs);
+}
+
+/**
+ * @param array $specs array de claves y funciones que construirán el objetivo
+ * @param object|null $tameme
+ * @return callable
+ */
 function unfold($specs, $tameme = null): callable
 {
     return function ($input, $optional = null) use ($specs, $tameme) {
@@ -77,8 +125,17 @@ function unfold($specs, $tameme = null): callable
     };
 }
 
-function isUnaryFn(callable $callable)
+/**
+ * @param callable $callable
+ * @return bool
+ * @throws \ReflectionException
+ * @todo implementar memoization para solo indagar una vez si el callable es unary
+ */
+function isUnaryFn($callable)
 {
+    if (!is_callable($callable)) {
+        return false;
+    }
     $reflector = new \ReflectionFunction($callable);
     return boolval($reflector->getNumberOfParameters() === 1);
 }
